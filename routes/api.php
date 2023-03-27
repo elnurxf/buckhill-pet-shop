d465d335f1f688a11d1075a820c773f9a1d0c597
Elnur Akhundov <elnurxf@gmail.com>
2023-03-27 17:36:42 +0400

    routes prep

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::get('/', function () {
+    return response()->noContent();
+});
+
+
+Route::prefix('v1')->namespace('v1')->name('v1.')->group(function () {
+
+    Route::get('/', function () {
+        return response([
+            'version'   => 1,
+            'name' => 'Buckhill Pet Shop API',
+        ], 200);
+    });
 });
